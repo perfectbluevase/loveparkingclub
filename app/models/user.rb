cb3d10640caaabc0970ecd7d8ccc1fc1d18bf03c
perfectbluevase <perfectbluevase@gmail.com>
2022-07-14 17:11:23 +0000

    [add]made_controller#parking

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   #Parkingのアソシエーション
-  has_many :parkings, dependent: :destory
+  has_many :parkings, dependent: :destroy
   #PostCommentのアソシエーション
   has_many :post_comments, dependent: :destroy
   #Bookmarkのアソシエーション
---
+++ test/controllers/public/parkings_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Public::ParkingsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

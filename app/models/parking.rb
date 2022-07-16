744550052d39a42e1b729453624b837b300907e4
perfectbluevase <perfectbluevase@gmail.com>
2022-07-16 15:19:34 +0000

    [add]Parking#view_show

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -12,7 +12,7 @@ class Parking < ApplicationRecord
     image
   end
   
-  #既にブックマークをしているかどうかを
+  #既にブックマークをしているかどうかを検証
   def bookmarked_by?(user)
     bookmarks.where(user_id: user).exists?
   end

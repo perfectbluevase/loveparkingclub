fcb83481e86ee356346043b58bae8572a82415c6
perfectbluevase <perfectbluevase@gmail.com>
2022-07-18 13:29:25 +0000

    [add]top_page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,17 @@
 class HomesController < ApplicationController
   def top
+    # hash_tagというURLパラメーターが存在していれば
+    @parkings = if params[:oreno_hash_tag].present?
+      tag = Tag.find_by(tag_name: params[:oreno_hash_tag])
+      if tag.present?
+        tag.parkings
+      else
+        flash.now[:notice] = "該当のタグはありません。"
+        Parking.none
+      end
+    else
+      Parking.all
+    end
   end
 
   def about

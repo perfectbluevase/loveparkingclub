b6a851bd18dc7e809470a640f8b12f0a5a6cd431
perfectbluevase <perfectbluevase@gmail.com>
2022-07-27 11:52:48 +0000

    [add]migration_rename_tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_064741) do
+ActiveRecord::Schema.define(version: 2022_07_27_113207) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
